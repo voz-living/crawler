615e6c8f3842b7b4991a55ac8e59d326bf73e1a9
Le-Khoi Phong <phong.lekhoi@gmail.com>
2016-11-13 02:44:53 +0800

    add datetime process

--- thread-crawler.js
+++ thread-crawler.js
@@ -1,8 +1,9 @@
 const program = require('commander');
 const cheerio = require('cheerio');
 
-const request = require('./utils/request');
-const generateUrl = require('./utils/generate-url');
+const request = require('utils/request');
+const generateUrl = require('utils/generate-url');
+const parseDateTime = require('utils/datetime').parse;
 
 program
   .version('1.0.0')
@@ -14,10 +15,59 @@ program
 const {thread: tid, page: pageNum, dryRun} = program;
 
 
+/**
+ * threadCrawler
+ * crawl thread
+ * @param {any} [tid=null]
+ * @param {any} [pageNum=-1]
+ * @param {boolean} [dryRun=false]
+ */
 async function threadCrawler(tid=null, pageNum = -1, dryRun=false) {
   if (tid === null) throw new Error('missing thread id');
   const html = await request(generateUrl.thread(tid, pageNum));
-  console.log(html.length);
+  const $ = cheerio.load(html);
+  const thread = parseThread($);
+}
+
+/**
+ * @param {any} $
+ */
+function parseThread($) {
+  // parse thread information
+  const {pages} = parseThreadInformation($);
+  const posts = [];
+  // parse posts
+  $('#posts > div').each(function(i, element) {
+    const $this = $(this);
+    parsePost($this);
+  });
+  return {};
+}
+
+
+/**
+ * @param {any} $
+ */
+function parseThreadInformation($) {
+  const $nav = $('.pagenav').eq(0).find('table .vbmenu_control').eq(0);
+  let pages = 1;
+  if($nav.length !== 0) {
+    const text = $nav.text();
+    const [, num] = text.match(/Page \d+ of (\d+)/);
+    pages = num;
+  }
+  return {pages};
+}
+
+function parsePost($) {
+  const $post = $.find('[id^="post"]');
+  const [, postId] = $post.attr('id').match(/post(\d+)/);
+
+  const $head = $post.find('td.thead')
+  const postNum = parseInt($head.find('[id^="postcount"]').text());
+  const datetimeStr = $head.find('> div').eq(1).text().trim();
+  const datetime = parseDateTime(datetimeStr); 
+  console.log(datetimeStr, datetime.toString());
 }
 
 threadCrawler(tid, pageNum, dryRun);
---
+++ utils/datetime.js
@@ -0,0 +1,52 @@
+/**
+ * return timezone offset in minute
+ * @return {Integer} offset
+ */
+function getTzOffset() {
+  const now = new Date();
+  const tz = now.getTimezoneOffset();
+  return tz + 420;
+}
+
+const offset = getTzOffset();
+
+/**
+ * get current datetime with timezone offsetment
+ * @return {Date} Date
+ */
+function getCurrent() {
+  const now = new Date();
+  now.setMinutes(offset);
+  return now;
+}
+
+/**
+ * @param {string} datetime
+ * @return {Date} parsed Datetime object
+ */
+function parseDateTime(datetime, {separation=','} = {}) {
+  const [dateStr, timeStr] = datetime.split(separation).map((s) => s.trim());
+  let date;
+  if (dateStr === 'Today') {
+    date = getCurrent();
+  } else if (dateStr === 'Yesterday') {
+    date = getCurrent();
+    date.setHours(date.getHours() - 24);
+  } else {
+    const ymd = dateStr.split('-').reverse();
+    date = new Date(...ymd);
+  }
+
+  const [h, m] = timeStr.split(':');
+  const finalDate = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    h, m, 0);
+  finalDate.setMinutes(finalDate.getMinutes() + offset);
+  return finalDate;
+}
+
+module.exports = {
+  parse: parseDateTime,
+};
